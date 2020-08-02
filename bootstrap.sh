857fd1c16aebb4c7d8eb46785af1d0fa61d167e3
Kei Shirakizawa <kei.shirakizawa@gmail.com>
2020-08-02 16:29:29 +0200

    bootstrap: Remove tasksel
    It turned out that tasksel didn't agree with AWS 's launch script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-# As an initial step, install git, ansible and friends. A Debian-based system
-# has tasksel, a built-in package management tool. Define a custom task and
-# install those multiple packages at once. Sometimes ansible from the main
-# archive is too old. A bootstrap playbook makes the system's conservative
-# ansible upgrade itself to the latest version. Subsequently proceed to the main
-# playbook with ansible-pull. This script must be run with an administrator
-# privilege and its usage is simply "$ sudo sh /path/to/bootstrap.sh".
-
+# As an initial step, install git, ansible and friends. Sometimes ansible from
+# the main archive is too old. A bootstrap playbook makes the system's
+# conservative ansible upgrade itself to the latest version. Subsequently
+# proceed to the main playbook with ansible-pull. This script must be run with
+# an administrator privilege and its usage is simply "$ sudo sh
+# /path/to/bootstrap.sh".
 
 set -e
 alias log='logger -s -t bootstrap.sh'
 
-DESCS_DIR="/usr/share/tasksel/descs"
-BOOT_TASK="${DESCS_DIR}/bootstrap.desc"
+export DEBIAN_FRONTEND=noninteractive
+export ANSIBLE_LOCAL_TEMP=$HOME/.ansible/tmp
+export ANSIBLE_REMOTE_TEMP=$HOME/.ansible/tmp
+
 ANSIBLE_VERSION="2.9"
 BOOT_PLAYBOOK="$(mktemp -t bootstrap_XXXXXXXXXX.yml)"
 MAIN_PLAYBOOK="main.yml"
@@ -26,37 +26,9 @@ apt-get update -y
 apt-get upgrade -y
 log "Made system up-to-date."
 
-# Check if tasksel is present in the system.
-if [ ! -x "$(command -v tasksel)" ]
-then
-    log "tasksel not found. Installing..."
-    apt-get install tasksel -y
-fi
-
-# Check write permission.
-if [ ! -w ${DESCS_DIR} ]
-then
-    log "${DESCS_DIR} doesn't exist or not writable. Exiting..."
-    exit 1
-fi
-
-cat <<EOF > ${BOOT_TASK}
-Task: bootstrap
-Relevance: 9
-Description: Bootstrap
- Install git, ansible and relevant utilities.
-Key:
-Packages: list
- git
- dirmngr
- ansible
-Section: user
-EOF
-
-log "Created ${BOOT_TASK}."
-
-tasksel install bootstrap
-log "Tasksel completed bootstrap task."
+# Install bootstrap packages.
+apt-get install -y git dirmngr ansible
+log "Installed bootstrap packages."
 
 cat <<EOF > ${BOOT_PLAYBOOK}
 ---
@@ -64,17 +36,17 @@ cat <<EOF > ${BOOT_PLAYBOOK}
 
   tasks:
 
-  - name: add an apt key by id from a keyserver
+  - name: Add an apt key by id from a keyserver
     apt_key:
       keyserver: keyserver.ubuntu.com
       id: 93C4A3FD7BB9C367
 
-  - name: add upstream repository of the latest ansible
+  - name: Add upstream repository of the latest ansible
     apt_repository:
       filename: ansible
       repo: deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
 
-  - name: ensure ansible is up to date
+  - name: Ensure ansible is up to date
     apt:
       name: ansible
       update_cache: yes
